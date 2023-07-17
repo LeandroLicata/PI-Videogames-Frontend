6b6358c025ba682415de6ba1bf03666a280bbfd1
LeandroLicata <leandrolicata1@gmail.com>
2023-07-17 17:11:45 -0300

    useVideogames update

--- src/hooks/useVideogames.js
+++ src/hooks/useVideogames.js
@@ -1,11 +1,21 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchVideogames } from "../features/videogame/videogameThunks";
+import { useParams, useNavigate } from "react-router-dom";
+import {
+  fetchVideogames,
+  fetchVideogameById,
+  deleteVideogame,
+} from "../features/videogame/videogameThunks";
+import Swal from "sweetalert2";
 
 const useVideogames = () => {
   const videogames = useSelector((state) => state.videogame.videogames);
   const videogameStatus = useSelector((state) => state.videogame.status);
   const dispatch = useDispatch();
+  const params = useParams();
+  const id = params.id;
+  const navigate = useNavigate();
+  const videogameDetail = useSelector((state) => state.videogame.detail);
 
   useEffect(() => {
     if (
@@ -17,9 +27,36 @@ const useVideogames = () => {
     }
   }, [dispatch, videogameStatus]);
 
+  useEffect(() => {
+    dispatch(fetchVideogameById(id));
+  }, [dispatch, id]);
+
+  const handleDeleteGame = () => {
+    dispatch(deleteVideogame(id))
+      .then(() => {
+        Swal.fire({
+          icon: "success",
+          title: "Game Deleted",
+          text: "The game has been successfully deleted",
+        }).then(() => {
+          navigate("/");
+        });
+      })
+      .catch(() => {
+        Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "An error occurred while deleting the video game.",
+        });
+      });
+  };
+
   return {
     videogames,
     videogameStatus,
+    id,
+    videogameDetail,
+    handleDeleteGame,
   };
 };
 
--- src/views/Detail/Detail.jsx
+++ src/views/Detail/Detail.jsx
@@ -1,61 +1,28 @@
-import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchVideogameById,
-  deleteVideogame,
-} from "../../features/videogame/videogameThunks";
-import { useEffect, useState } from "react";
-import { useParams, NavLink, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import useVideogames from "../../hooks/useVideogames";
 import Loading from "../../components/Loading/Loading";
 import Error from "../../components/Error/Error";
 import Screenshots from "../../components/Screenshots/Screenshots";
 import parse from "html-react-parser";
-import Swal from "sweetalert2";
 
 export default function Detail() {
-  const dispatch = useDispatch();
-  const videogameStatus = useSelector((state) => state.videogame.status);
-  const params = useParams();
-  const id = params.id;
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    dispatch(fetchVideogameById(id));
-  }, [dispatch, id]);
-
-  const videogame = useSelector((state) => state.videogame.detail);
-
-  const handleDeleteGame = () => {
-    dispatch(deleteVideogame(id))
-      .then(() => {
-        Swal.fire({
-          icon: "success",
-          title: "Game Deleted",
-          text: "The game has been successfully deleted",
-        }).then(() => {
-          navigate("/");
-        });
-      })
-      .catch(() => {
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          text: "An error occurred while deleting the video game.",
-        });
-      });
-  };
+  const { videogameStatus, id, videogameDetail, handleDeleteGame } =
+    useVideogames();
 
   return videogameStatus === "loading" ? (
     <Loading />
   ) : videogameStatus === "failed" ? (
     <Error />
-  ) : videogame !== null ? (
+  ) : videogameDetail !== null ? (
     <div className="container card border-info my-2">
-      <h2 className="text-center text-secondary mt-3">{videogame.name}</h2>
+      <h2 className="text-center text-secondary mt-3">
+        {videogameDetail.name}
+      </h2>
       <div className="row mt-4">
         <div className="col-12 container">
           <div className="text-center">
             <img
-              src={videogame.background_image || "/images/no-signal.avif"}
+              src={videogameDetail.background_image || "/images/no-signal.avif"}
               alt="image"
               className="img-fluid"
             />
@@ -63,21 +30,21 @@ export default function Detail() {
         </div>
         <div className="col-12 mt-4">
           <p className="text-info">Platforms</p>
-          <p className="">{videogame.platforms.join(", ")}</p>
+          <p className="">{videogameDetail.platforms.join(", ")}</p>
           <p className="text-info">Genres</p>
-          <p className="">{videogame.genres.join(", ")}</p>
+          <p className="">{videogameDetail.genres.join(", ")}</p>
           <p className="text-info">Release Date</p>
-          <p className="">{videogame.released}</p>
+          <p className="">{videogameDetail.released}</p>
           <p className="text-info">Rating</p>
-          <p className="">★{videogame.rating}</p>
+          <p className="">★{videogameDetail.rating}</p>
         </div>
         <div className="col-md-12 mt-4">
           <p className="text-info">Description</p>
-          <div className="">{parse(videogame.description)}</div>
+          <div className="">{parse(videogameDetail.description)}</div>
         </div>
       </div>
-      {videogame.screenshots?.length ? (
-        <Screenshots videogameScreenshots={videogame.screenshots} />
+      {videogameDetail.screenshots?.length ? (
+        <Screenshots videogameScreenshots={videogameDetail.screenshots} />
       ) : null}
       <NavLink to="/" className="btn btn-outline-secondary my-2" role="button">
         Back
